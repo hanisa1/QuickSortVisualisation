ec396c6f79aa8541874495f853d05715f7e80a01
hanisa1 <hanisamohamed1@gmail.com>
2024-09-03 23:45:51 +1000

    Initial commit

---
+++ QuickSortVisualiser/QuickSortVisualiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickSortVisualiserApp.swift
+//  QuickSortVisualiser
+//
+//  Created by Hanisa Hilole on 3/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct QuickSortVisualiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
